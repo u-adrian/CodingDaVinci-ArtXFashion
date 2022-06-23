211d54618807ce8fe841a1f32ee3b8931827b63c
u-adrian <94900495+u-adrian@users.noreply.github.com>
2022-06-23 17:32:00 +0200

    Will create necessary folders

--- django-webserver/run_commands.sh
+++ django-webserver/run_commands.sh
@@ -1,2 +1,5 @@
+#!/bin/sh
 docker-compose run web python3 manage.py makemigrations imageupload
 docker-compose run web python3 manage.py migrate imageupload
+mkdir -p ./images/segmentation
+mkdir -p ./images/style
